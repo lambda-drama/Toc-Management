fa0605754ffbad81d0892864ec79a3fe2eca1cb3
maniamartial <martialmania19@gmail.com>
2025-11-11 00:02:12 +0300

    feat: Update the dates

--- toc_management/toc_management/doctype/property_management/property_management.js
+++ toc_management/toc_management/doctype/property_management/property_management.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, martialmania19@gmail.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Property Management", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Property Management", {
+	agreement_date(frm) {
+		// When agreement_date is entered, set is_active to 1
+		if (frm.doc.agreement_date && !frm.doc.handback_date) {
+			frm.set_value("is_active", 1);
+		}
+	},
+	
+	handback_date(frm) {
+		// When handback_date is entered, set is_active to 0
+		if (frm.doc.handback_date) {
+			frm.set_value("is_active", 0);
+		} else if (frm.doc.agreement_date) {
+			// If handback_date is cleared and agreement_date exists, set is_active to 1
+			frm.set_value("is_active", 1);
+		}
+	},
+	
+	is_active(frm) {
+		// If user tries to set is_active to 1 but handback_date exists, force it to 0
+		if (frm.doc.is_active && frm.doc.handback_date) {
+			frappe.msgprint(__("Cannot set Is Active to 1 when Handback Date is set."));
+			frm.set_value("is_active", 0);
+		}
+	}
+});
--- toc_management/toc_management/doctype/property_management/property_management.py
+++ toc_management/toc_management/doctype/property_management/property_management.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, martialmania19@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class PropertyManagement(Document):
-	pass
+	def validate(self):
+		# If handback_date exists, is_active must be 0
+		if self.handback_date:
+			self.is_active = 0
+	
+	def before_save(self):
+		# When agreement_date is entered, set is_active to 1
+		if self.agreement_date and not self.handback_date:
+			self.is_active = 1
+		
+		# When handback_date is entered, set is_active to 0
+		if self.handback_date:
+			self.is_active = 0
